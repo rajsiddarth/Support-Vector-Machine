a3b1b045d4853a8ae46474f8de830af290c29f19
Siddarth <rajasiddartha94@gmail.com>
2017-03-20 16:44:56 +0530

    adding files

---
+++ Readme.txt
@@ -0,0 +1,19 @@
+1. Classifying loan defaulters in Bank data set using SVM
+The Bank data set used contains 12 features, 5000 samples with Personal loan as target variable.
+The independent features are
+ID: Customer ID
+Age: Customer Age
+Experience: #years of Professional experience
+Income: Annual Income of the customer in $000
+ZIP Code:Home address Zip Code
+Family: Family size of the customer 
+CCAvg: Avg spending on credit cards per month in $000
+Education: Education level 1:Undergrad 2: Graduate 3:Advanced/Professional 
+Mortgage: Value of mortgage if any $000
+Securities Account: Does the customer have a securities account with the bank?
+CD Account:Does the customer have a certificate of deposit account with the bank?
+Online : Does the customer use internet banking facilities?
+CreditCard: Does the customer use a credit card issued by the respective bank?
+Personal Loan : Did the customer accept the personal loan offered in the last campaign? 
+
+2. 
---
+++ SVM.R
@@ -0,0 +1,33 @@
+rm(list=ls(all=T))
+data=read.csv("UniversalBank.csv")
+str(data)
+drop_atr=c("ID","ZIP.Code","Experience")
+names=setdiff(colnames(data),drop_atr)
+final_data=data[names]
+str(final_data)
+set.seed(123)
+library(caret)
+library(MASS)
+temp=sample.split(final_data$Personal.Loan,SplitRatio = 0.7)
+train=final_data[temp,]
+test=final_data[!temp,]
+ind_Atr=setdiff(colnames(train),"Personal.Loan")
+table(train$Personal.Loan)
+table(test$Personal.Loan)
+model=svm(train[ind_Atr],train$Personal.Loan,type="C-classification",kernel="linear",cost=1,gamma=0.1)
+summary(model)
+pred=predict(model,train[ind_Atr])
+summary(pred)
+names(pred)
+acc_train=table(train$Personal.Loan,pred)
+accuracy=sum(diag(acc_train))/sum(acc_train)
+pred_test=predict(model,test[ind_Atr])
+pred_test2=table(test$Personal.Loan,pred_test)
+acc_test=sum(diag(pred_test2))/sum(pred_test2)
+#svm regression
+rm(list=ls(all=T))
+
+data2=read.csv("BostonHousing.csv")
+str(data2)
+temp2=sample
+model2=svm()
